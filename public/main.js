023a27c249dbc4dbb04c9627fa6c4711c1c367ff
Seth Keefer <keeferseth132@gmail.com>
2015-08-05 09:59:01 -0600

    dropdown added

--- public/main.js
+++ public/main.js
@@ -2,20 +2,55 @@ $(document).ready(function () {
 
 // 4 variables below connect to the Web Audio API.
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var audioElement = document.getElementById('audioElement');
-  var audioSrc = audioCtx.createMediaElementSource(audioElement);
-  var analyser = audioCtx.createAnalyser();
 
-  // Bind our analyser to the media element source.???
-  audioSrc.connect(analyser);
-  audioSrc.connect(audioCtx.destination);
+  var currentlyPlaying = null;
+
+  $('#songSelection').on('change', function(){
+    if(currentlyPlaying === null){
+      elementSources[$(this).val()].play()
+      currentlyPlaying = elementSources[$(this).val()]
+    }
+    else{
+      currentlyPlaying.pause()
+      elementSources[$(this).val()].play()
+      currentlyPlaying = elementSources[$(this).val()]
+    }
+  })
+
+  $('#stop').on('click', function(){
+    currentlyPlaying.pause()
+    currentlyPlaying = null
+  })
+
+  var elementSources = {
+     audioElement1 : document.getElementById('audioElement1'),
+     audioElement2 : document.getElementById('audioElement2'),
+     audioElement3 : document.getElementById('audioElement3'),
+     audioElement4 : document.getElementById('audioElement4'),
+     audioElement5 : document.getElementById('audioElement5')}
+
+  var sources = { 
+     audioSrc1 : audioCtx.createMediaElementSource(audioElement1),
+     audioSrc2 : audioCtx.createMediaElementSource(audioElement2),
+     audioSrc3 : audioCtx.createMediaElementSource(audioElement3),
+     audioSrc4 : audioCtx.createMediaElementSource(audioElement4),
+     audioSrc5 : audioCtx.createMediaElementSource(audioElement5)}
+     analyser = audioCtx.createAnalyser();
+
+    // Bind our analyser to the media element source.???
+    sources.audioSrc1.connect(analyser);
+    sources.audioSrc2.connect(analyser);
+    sources.audioSrc3.connect(analyser);
+    sources.audioSrc4.connect(analyser);
+    sources.audioSrc5.connect(analyser);
+    analyser.connect(audioCtx.destination);
 
   //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
   var frequencyData = new Uint8Array(750); // Uint8Array is array of 8bit integers & param is number of bars. Has a threshold.
 
   var svgHeight = '300'; // there is a bug, quotes denote pixle count
   var svgWidth = '1600';
-  var barPadding = '0.5';
+  var barPadding = '1';
 
   function createSvg(parent, height, width) {
     return d3.select(parent).append('svg').attr('height', height).attr('width', width);
@@ -61,5 +96,4 @@ $(document).ready(function () {
 
   // Run the loop
   renderChart();
-
 });
